71c35d179e71584f3f5fdd6d798392ad5e5ee647
duguangyan <dis2010@126.com>
2018-09-28 22:27:48 +0800

    cc

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,9 @@
 <?php
+header('content-type:application:json;charset=utf8');
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Methods:GET, POST, PATCH, PUT, OPTIONS');
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
+
 
 /*
 |--------------------------------------------------------------------------
