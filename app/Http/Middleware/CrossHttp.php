622918b5e28316974e3da9e7546939ce1f9ceb55
duguangyan <dis2010@126.com>
2018-09-28 20:22:06 +0800

    c

--- app/Http/Middleware/CrossHttp.php
+++ app/Http/Middleware/CrossHttp.php
@@ -15,11 +15,12 @@ class CrossHttp
      */
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
-        $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-        // $response->header('Access-Control-Allow-Credentials', 'true');
-        return $response;
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Credentials: true");
+        header("Access-Control-Allow-Methods: *");
+        header("Access-Control-Allow-Headers: Content-Type,Access-Token");
+        header("Access-Control-Expose-Headers: *");
+
+        return $next($request);
     }
 }
