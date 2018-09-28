b06229d86ee2d4a39b2bc86af379e0cb223a100b
duguangyan <dis2010@126.com>
2018-09-28 17:48:07 +0800

    x

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // 指定允许其他域名访问
+        header('Access-Control-Allow-Origin:*');
+// 响应类型
+        header('Access-Control-Allow-Methods:POST');
+// 响应头设置
+        header('Access-Control-Allow-Headers:x-requested-with,content-type');
+
         //
         Schema::defaultStringLength(191);
         $this->app->singleton(FakerGenerator::class, function() {
