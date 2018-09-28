99e413e15285b55771414a62060268cb03848217
duguangyan <dis2010@126.com>
2018-09-28 22:33:57 +0800

    cc

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,6 +42,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            'cors'
         ],
     ];
 
@@ -62,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'login'=>\App\Http\Middleware\BeforeUserLoginMiddleware::class,
+        'cors' => \App\Http\Middleware\CrossHttp::class,
     ];
 }
